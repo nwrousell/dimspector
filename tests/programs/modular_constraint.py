61104c6500e8acf48d18a69772d4131628dd8e82
Praccho Muna-McQuay <pracchommc@gmail.com>
2025-12-04 18:44:29 -0500

    addition to illustrative example

--- tests/programs/modular_constraint.py
+++ tests/programs/modular_constraint.py
@@ -13,6 +13,14 @@ in the following, should we either:
 def matmul(a: T["a b"], b: T["c d"]) -> T["a d"]:
     return a @ b
 
+"""
+in the following, in a world w/ constraints, if the user tells us b has shape [b, c], we may under the hood
+still give a and b distinct dimvars [a, b], [c, d], but just immediately introduce the constraint
+b <=> c b/c the user typed b as [b, c]
+"""
+def matmul_explicit(a: T["a b"], b: T["b c"]) -> T["a c"]:
+    return a @ b
+
 def f():
     x = torch.ones(( 5, 10)) # => Shape( 5, 10)
     y = torch.ones((15, 20)) # => Shape(20, 15)
