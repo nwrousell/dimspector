f1a15737e1a50e8dd5b028e5d1eabea5a0a0144e
Praccho Muna-McQuay <pracchommc@gmail.com>
2025-12-04 18:40:09 -0500

    modular constraint illustrative example

---
+++ tests/programs/modular_constraint.py
@@ -0,0 +1,19 @@
+from typing import Generic
+import torch
+
+
+class T(Generic(str)): ...
+
+"""
+in the following, should we either:
+- have the user tell us b <=> c explicitly through the signature OR
+- produce a constraint ourselves that b <=> c, which should be propagated
+  to the caller mapped back to their arguments' shapes
+"""
+def matmul(a: T["a b"], b: T["c d"]) -> T["a d"]:
+    return a @ b
+
+def f():
+    x = torch.ones(( 5, 10)) # => Shape( 5, 10)
+    y = torch.ones((15, 20)) # => Shape(20, 15)
+    z = matmul(x, y)         # => deduced as bad because matmul expects b <=> c ==> 10 <=> 20 (?)
