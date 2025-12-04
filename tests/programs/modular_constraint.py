63fdf8f963152c86e2c81e34989a6e97bdd9216a
Praccho Muna-McQuay <pracchommc@gmail.com>
2025-12-04 18:40:31 -0500

    typo

--- tests/programs/modular_constraint.py
+++ tests/programs/modular_constraint.py
@@ -8,7 +8,7 @@ class T(Generic(str)): ...
 in the following, should we either:
 - have the user tell us b <=> c explicitly through the signature OR
 - produce a constraint ourselves that b <=> c, which should be propagated
-  to the caller mapped back to their arguments' shapes
+  to the caller and mapped back to their arguments' shapes
 """
 def matmul(a: T["a b"], b: T["c d"]) -> T["a d"]:
     return a @ b
